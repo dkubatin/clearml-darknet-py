5954875d720b50107ff246548a2882257ee80c1d
Denis Kubatin <kubatin_denis@mail.ru>
2022-05-06 18:08:37 +0300

    upd version 0.1.2 → 0.1.3

--- clearml_darknet/__init__.py
+++ clearml_darknet/__init__.py
@@ -1,4 +1,4 @@
 from .darknet import Darknet
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __author__ = 'Denis Kubatin'
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='clearml-darknet-py',
-    version='0.1.2',
+    version='0.1.3',
     description='Python package for training neural networks through the Darknet framework in ClearML',
     long_description=long_description,
     long_description_content_type="text/markdown",
